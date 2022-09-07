d4bdfab6bcd7c1d061e0bcd7bdcc107827e09670
Mindy Medeiros <mmedei6@wgu.edu>
2022-09-07 03:18:46 -0400

    Added some example methods for strings, and tried out length

--- script.py
+++ script.py
@@ -4,3 +4,15 @@
 quote = "All is fair in love and war."
 
 print(quote)
+
+# Print can be in upper case via ".upper()"
+print(quote.upper())
+
+# as well as lower via ".lower()"
+print(quote.lower())
+
+# and Title Case ".title()"
+print(quote.title())
+
+# I can also print the length!
+print(len(quote))
